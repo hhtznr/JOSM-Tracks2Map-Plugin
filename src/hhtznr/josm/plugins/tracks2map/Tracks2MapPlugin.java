e2b03a3950d51a185b0f224b6beb51b0d9c8863e
Harald Hetzner <57875126+hhtznr@users.noreply.github.com>
2023-11-27 20:44:45 +0100

    Create initial JOSM plugin configuration

---
+++ src/hhtznr/josm/plugins/tracks2map/Tracks2MapPlugin.java
@@ -0,0 +1,17 @@
+package hhtznr.josm.plugins.tracks2map;
+
+import org.openstreetmap.josm.plugins.Plugin;
+import org.openstreetmap.josm.plugins.PluginInformation;
+
+/**
+ * Plugin class for opening all GPX tracks in the current map view.
+ *
+ * @author Harald Hetzner
+ */
+public class Tracks2MapPlugin extends Plugin {
+
+    public Tracks2MapPlugin(PluginInformation info) {
+        super(info);
+    }
+
+}
